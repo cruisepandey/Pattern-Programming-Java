8063ac6758dc371fee4de351203d3b16fad707a1
cruisepandey <pandeyabhishek895@gmail.com>
2018-02-26 12:47:43 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,25 @@
+package com.practice.all;
+
+public class Pattern2 {
+	
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1; i<=5 ;i++){
+			for(int j = 1 ; j<=n-i ; j++ ){
+				System.out.print(" ");
+			}
+			for(int k =1;k<=i; k++){
+				System.out.print("*");
+			}
+			System.out.println();
+	 }
+  }
+}
+
+//o/p is : 
+
+/*    *
+     **
+    ***
+   ****
+  *****    */
